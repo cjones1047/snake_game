5dd10107cd7daed1105e9128a902e05bf487cb72
cjones1047 <cjmj1047@gmail.com>
2022-11-28 13:43:37 -0600

    Canvas starts with snake that is 3 turtles wide, each turtle being 20 pixels x 20 pixels

--- main.py
+++ main.py
@@ -5,4 +5,13 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 
+all_turtles = []
+starting_position = (0, 0)
+
+for i in range(3):
+    new_turtle = Turtle(shape="square")
+    new_turtle.color("white")
+    new_turtle.goto(starting_position[0] - (i * 20), starting_position[1])
+    all_turtles.append(new_turtle)
+
 screen.exitonclick()
