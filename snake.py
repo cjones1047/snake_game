28de555ff15c3fd6ec96f8d4fbfbdba85bd66699
cjones1047 <cjmj1047@gmail.com>
2022-11-28 14:41:59 -0600

    Snake can not reverse direction and run into itself

--- snake.py
+++ snake.py
@@ -1,5 +1,9 @@
 from turtle import Turtle
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 
 class Snake:
@@ -7,6 +11,7 @@ class Snake:
     def __init__(self):
         self.snake = []
         self.create_snake()
+        self.head = self.snake[0]
 
     def create_snake(self):
         for i in range(3):
@@ -22,16 +27,20 @@ class Snake:
             new_y = self.snake[segment - 1].ycor()
             self.snake[segment].goto(new_x, new_y)
 
-        self.snake[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
 
     def up(self):
-        self.snake[0].setheading(90)
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
 
     def down(self):
-        self.snake[0].setheading(270)
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
 
     def left(self):
-        self.snake[0].setheading(180)
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
 
     def right(self):
-        self.snake[0].setheading(0)
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
