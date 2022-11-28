bbc360acc567335a81e11a5252a598b5daf4172f
cjones1047 <cjmj1047@gmail.com>
2022-11-28 14:35:25 -0600

    Snake correctly responds by changing directions to key presses for 'up', 'down', 'left', and 'right'

--- main.py
+++ main.py
@@ -14,7 +14,11 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
+    screen.listen()
+    screen.onkey(snake.up, "Up")
+    screen.onkey(snake.down, "Down")
+    screen.onkey(snake.left, "Left")
+    screen.onkey(snake.right, "Right")
     snake.move()
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -23,3 +23,15 @@ class Snake:
             self.snake[segment].goto(new_x, new_y)
 
         self.snake[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        self.snake[0].setheading(90)
+
+    def down(self):
+        self.snake[0].setheading(270)
+
+    def left(self):
+        self.snake[0].setheading(180)
+
+    def right(self):
+        self.snake[0].setheading(0)
