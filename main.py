ffbc9730fdbd4914b6bd7f194be761cf10283a31
cjones1047 <cjmj1047@gmail.com>
2022-11-28 13:55:39 -0600

    Snake automatically moves forward after being drawn

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
+import time
 from turtle import Screen, Turtle
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+screen.tracer(0)
 
-all_turtles = []
+snake = []
 starting_position = (0, 0)
 
 for i in range(3):
-    new_turtle = Turtle(shape="square")
-    new_turtle.color("white")
-    new_turtle.goto(starting_position[0] - (i * 20), starting_position[1])
-    all_turtles.append(new_turtle)
+    new_segment = Turtle(shape="square")
+    new_segment.color("white")
+    new_segment.penup()
+    new_segment.goto(starting_position[0] - (i * 20), starting_position[1])
+    snake.append(new_segment)
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    for segment in snake:
+        segment.forward(20)
 
 screen.exitonclick()
