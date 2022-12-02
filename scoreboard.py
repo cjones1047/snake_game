5427cc718520fd075cc9e34780ff9d3803626f80
cjones1047 <cjmj1047@gmail.com>
2022-12-02 13:07:06 -0600

    Altered Scoreboard class by adding reset method to keep track of new high_score attribute over time

--- scoreboard.py
+++ scoreboard.py
@@ -13,6 +13,7 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.sety(TOP)
         self.score = 0
+        self.high_score = 0
         self.update_scoreboard()
 
     def increase(self):
@@ -21,8 +22,14 @@ class Scoreboard(Turtle):
 
     def update_scoreboard(self):
         self.clear()
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score} | High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.sety(0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    # def game_over(self):
+    #     self.sety(0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
